887a40a6f990f40dad87c1c57c6d4d59ea2cc1d2
tomganchos <tomganchos@mail.ru>
2020-05-04 22:42:58 +0300

    Add animation icon

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'App',
+    data () {
+      return {
+        test: 'test',
+      }
     }
   }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background: linear-gradient(180deg, #edfdf4 0%, #FFFFFF 100%);
+  height: 80vh;
+  text-align: center;
+  text-align: -webkit-center;
+  text-align: -moz-center;
 }
+
 </style>
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header">
+      <div class="header-icon">
+        <icon-tree/>
+      </div>
+      <div class="header-label">
+        <div class="header-label__text">
+          <div>Твоя</div><div>Родословная</div>
+        </div>
+      </div>
+      <div class="header-lang">
+        Lang
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import IconTree from "@/views/IconTree";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'Landing',
+  components: {IconTree},
+  data () {
+    return {
+      test: 'test',
+    }
   }
 }
 </script>
+
+<style scoped>
+  .container {
+    max-width: 1200px;
+  }
+  .header {
+    display: flex;
+    padding: 10px;
+  }
+  .header .header-label {
+    display: flex;
+    flex-grow: 1;
+    color: #1b5e20;
+    padding-left: 10px;
+    font-size: 170%;
+    text-align: left;
+  }
+  .header .header-label__text {
+    display: flex;
+    flex-direction: column;
+    height: 64px;
+    align-items: flex-start;
+  }
+</style>
